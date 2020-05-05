fd939b67740ff3db4c52a2e08c7ee404d40ef0b6
JoseMancera <jose.mancera@beekeeper.io>
2020-05-05 21:02:51 +0200

    Process data based on ETL is done

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,61 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    It loads the data and merge the messages and categories datasets based on the unique id
+    @:param messages_filepath
+    @:param categories_filepath
+    @:return merged dataset based on ids
+
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    It receives a raw dataset
+    :param df: raw dataframe
+    :return: Transformed dataframe with features ready for the ML Pipeline
+    """
+
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+    row = categories.head(1)
+    # apply a lambda function that takes everything up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :].tolist()
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    It saves the dataframe as as sql database
+
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('disaster_messages', engine, index=False)
 
 
 def main():
